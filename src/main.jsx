bae12b5705332ecf5543120532900c967f08fe0b
shindaraphone <shineststar00@gmail.com>
2026-08-23 12:00:23 +0100

    Add Admin component for admin page routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
+import Admin from "./Admin.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const isAdminPage =
+  window.location.pathname.startsWith("/admin");
+
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
   <React.StrictMode>
-    <App />
+    {isAdminPage ? <Admin /> : <App />}
   </React.StrictMode>
 );
