6278eaaa5ea15542046e49c071ec4e2fa1e05fc0
shindaraphone <shineststar00@gmail.com>
2026-08-26 09:04:55 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import App from "./App.jsx";
 import Admin from "./Admin.jsx";
+import AdminLogin from "./AdminLogin.jsx";
+import ProtectedAdmin from "./ProtectedAdmin.jsx";
 import PrivacyPolicy from "./PrivacyPolicy.jsx";
 import TermsOfService from "./TermsOfService.jsx";
 import "./index.css";
-
 const path = window.location.pathname;
-
-const isAdminPage = path.startsWith("/admin");
-const isPrivacyPage = path === "/privacy-policy";
-const isTermsPage = path === "/terms";
-
+/*
+========================================
+ROUTES
+========================================
+*/
+const isAdminLoginPage =
+  path === "/admin-login";
+const isAdminPage =
+  path === "/admin";
+const isPrivacyPage =
+  path === "/privacy-policy";
+const isTermsPage =
+  path === "/terms";
+/*
+========================================
+RENDER
+========================================
+*/
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
   <React.StrictMode>
-    {isAdminPage ? (
-      <Admin />
+    {isAdminLoginPage ? (
+      <AdminLogin />
+    ) : isAdminPage ? (
+      <ProtectedAdmin>
+        <Admin />
+      </ProtectedAdmin>
     ) : isPrivacyPage ? (
       <PrivacyPolicy />
     ) : isTermsPage ? (
