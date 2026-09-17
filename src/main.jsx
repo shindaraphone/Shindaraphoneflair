602919a0db66068c753da97bad6063d6c2adc318
shindaraphone <shineststar00@gmail.com>
2026-09-17 22:34:48 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,6 @@ if ("serviceWorker" in navigator) {
 
 import App from "./App.jsx";
 import "./shindara-redesign.css";
-import "./midnight-purple.css";
 import "./system-theme.css";
 import ProtectedAdmin from "./ProtectedAdmin.jsx";
 import PrivacyPolicy from "./PrivacyPolicy.jsx";
